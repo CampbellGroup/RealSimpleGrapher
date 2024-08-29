8799674f9915ea2da59a62cc8a61dfcdd07541d5
thomasdellaert <dellaert.thomas@gmail.com>
2024-08-29 14:45:23 -0700

    py3 compatibility

--- depricated/GraphWidget.py
+++ depricated/GraphWidget.py
@@ -61,7 +61,7 @@ class Graph(QtGui.QWidget):
 
     def update_figure(self, _input=None):
         artists = []
-        for ident, (artist, dataset, index) in self.artists.iteritems():
+        for ident, (artist, dataset, index) in self.artists.items():
             x = dataset.data[:, 0]
             y = dataset.data[:, index + 1]
             artist.set_data((x, y))
@@ -89,7 +89,7 @@ class Graph(QtGui.QWidget):
         self.canvas.draw()
 
     def checkboxChanged(self, state):
-        for ident, item in self.tracelist.trace_dict.iteritems():
+        for ident, item in self.tracelist.trace_dict.items():
             if item.checkState():
                 self.display(ident, True)
             else:
